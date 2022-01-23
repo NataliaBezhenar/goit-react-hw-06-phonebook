9ec92a141c2d9b35d81c7b9bdfa4e72f631c5f60
Natalia Bezhenar <natalia.bezhenar@gmail.com>
2022-01-23 15:03:14 +0200

    persist edit

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,10 @@ import {
   getDefaultMiddleware,
   combineReducers,
 } from "@reduxjs/toolkit";
+import logger from "redux-logger";
 import {
+  persistStore,
+  persistReducer,
   FLUSH,
   REHYDRATE,
   PAUSE,
@@ -11,22 +14,38 @@ import {
   PURGE,
   REGISTER,
 } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import { contactsReducer } from "./contact/contact-reducers";
 import { filterReducer } from "./filter/filter-reducer";
 
-const middleware = getDefaultMiddleware({
-  serializableCheck: {
-    ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-  },
-});
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+  logger,
+];
+
+const persistConfig = {
+  key: "contacts",
+  storage,
+  blacklist: ["filter"],
+};
 
 const rootReducer = combineReducers({
   contacts: contactsReducer,
   filter: filterReducer,
 });
 
-export const store = configureStore({
-  reducer: rootReducer,
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
   middleware,
   devTools: process.env.NODE_ENV === "development",
 });
+
+const persistor = persistStore(store);
+
+export { store, persistor };
